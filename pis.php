ce0fdb9e250508a2cce63bfcbebcc90488c26a4a
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2019-10-02 17:27:41 +0530

    Update pis.php

--- pis.php
+++ pis.php
@@ -1,44 +1,44 @@
-<html>
+ <html>
 <head>
 <style>
 table td{
- text-align:center;
- padding: 15px;
- background: white;
- width: 700px;
+text-align:center;
+padding: 15px;
+background: white;
+width: 700px;
 }
 
 
 table th{
- text-align:center;
- font-family: "ariel";
- padding: 15px;
- background: white;
- width: 700px;
- font-size: 30px;
- text-transform: uppercase;
- letter-spacing: 13px;
+text-align:center;
+font-family: "ariel";
+padding: 15px;
+background: white;
+width: 700px;
+font-size: 30px;
+text-transform: uppercase;
+letter-spacing: 13px;
 }
 
 div{
-	background: white;
-	width: 1350px;
-	height: auto;
-	border: 12px blue;
-	padding: 50px;
-	margin 20px;
-	border-radius: 15px;
-	margin-left: auto;
-	margin-right: auto;
-	margin-top: 10px;
-	border: 10px solid black;
-    
+background: white;
+width: 1350px;
+height: auto;
+border: 12px blue;
+padding: 50px;
+margin 20px;
+border-radius: 15px;
+margin-left: auto;
+margin-right: auto;
+margin-top: 10px;
+border: 10px solid black;
+
 }
 
 body.welcome {
-  background: #0028ff;
+ background: #0028ff;
 
- 
+
 }
 input.MyButton {
 width: 300px;
@@ -60,26 +60,26 @@ border: 1px solid #fff;
 }
 
 p {
-	color:white;
-	text-align: center;
-	text-transform: uppercase;
-	letter-spacing: 13px;
-	font-family: "Ariel";
-	font-size: 24px;
+color:white;
+text-align: center;
+text-transform: uppercase;
+letter-spacing: 13px;
+font-family: "Ariel";
+font-size: 24px;
 }
 
 .welcome #welcome {
-  background: #311b92 ;
-  width: 56px;
-  height: 56px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  overflow: hidden;
-  opacity: 0;
-  transform: translate(-50%, -50%);
-  border-radius: 50%;
-  animation: init 0.5s 0.2s cubic-bezier(0.55, 0.055, 0.675, 0.19) forwards, moveDown 1s 0.8s cubic-bezier(0.6, -0.28, 0.735, 0.045) forwards, moveUp 1s 1.8s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards, materia 0.5s 2.7s cubic-bezier(0.86, 0, 0.07, 1) forwards, hide 2s 2.9s ease forwards;
+ background: #311b92 ;
+ width: 56px;
+ height: 56px;
+ position: absolute;
+ left: 50%;
+ top: 50%;
+ overflow: hidden;
+ opacity: 0;
+ transform: translate(-50%, -50%);
+ border-radius: 50%;
+ animation: init 0.5s 0.2s cubic-bezier(0.55, 0.055, 0.675, 0.19) forwards, moveDown 1s 0.8s cubic-bezier(0.6, -0.28, 0.735, 0.045) forwards, moveUp 1s 1.8s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards, materia 0.5s 2.7s cubic-bezier(0.86, 0, 0.07, 1) forwards, hide 2s 2.9s ease forwards;
 }
 
 </style>
@@ -87,60 +87,63 @@ p {
 
 <body class="welcome">
 <span id="splash-overlay" class="splash"></span>
-  <span id="welcome"></span>
+ <span id="welcome"></span>
 
 <p> data insertion</p>    
-	  <div>
+ <div>
 
 
 <?php
 
-$con=mysqli_connect("localhost","root"," ","student");
+$con=mysqli_connect("localhost","root","","student");
 // Check connection
 if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
 $t1= filter_input(INPUT_POST,'t1');
 $spec=filter_input(INPUT_POST,'spec');
-$img=filter_input(INPUT_POST,'img');
 
-$a= "INSERT INTO sample2(bikename,bikeinfo,image) values ('$t1', '$spec','$img')";
+
+$a= "INSERT INTO sample2(bikename,bikeinfo) values ('$t1', '$spec') ";
+
 
 if ($con->query($a) === TRUE) {
-    echo " data inserted!";
+echo " data inserted!";
 } else {
-    echo "Error: " . $a . "<br>" . $con->error;
+echo "Error: " . $a . "<br>" . $con->error;
 }
-	
+
 $name ='bikename';
 $info ='bikeinfo';
-$imag='image';
 
-$sql="SELECT bikename,bikeinfo,image FROM sample2 ORDER BY bikename";
+$sql="SELECT bikename,bikeinfo FROM sample2 ORDER BY bikename";
 
 
 if ($result=mysqli_query($con,$sql))
-  {
-  echo "<table width='100%'>";
-  echo "<tr>";
-  echo "<th>" . $name . "</th>";
-  echo "<th>" . $info . "</th>";
-  echo "<th>" . $imag . "</th>";
-  echo "</tr>";
-  // Fetch one and one row
-  while ($row=mysqli_fetch_row($result))
-    {
-  echo "<table border ='1', width='100%', height= '200' border>" ;
-  echo "<tr>";
-  echo "<td>" . $row[0] . "</td>";
-  echo "<td>" . $row[1] . "</td>";
-  echo "<td>" . $row[2] . "</td>";
-  echo "</tr>";
-  echo "</table>";
-    }
-  // Free result set
-  mysqli_free_result($result);
+ {
+	 
+	 
+ echo "<table width='100%'>";
+ echo "<tr>";
+ echo "<th>" . $name . "</th>";
+ echo "<th>" . $info . "</th>";
+ echo "</tr>";
+ // Fetch one and one row
+ while ($row=mysqli_fetch_row($result))
+{
+ echo "<table border ='1', width='100%', height= '200' border>" ;
+ echo "<tr>";
+ echo "<td>" . $row[0] . "</td>";
+ echo "<td>" . $row[1] . "</td>";
+ echo "</tr>";
+ echo "</table>";
+ 
+}
+
+
+ // Free result set
+ mysqli_free_result($result);
 }
 mysqli_close($con);
 ?>
